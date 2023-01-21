964e8eed397e0d027b26037640737905a948f731
nourHalhoul <nourhalhoul@gmail.com>
2023-01-21 07:56:46 -0500

    Fixed URLs with "#"

--- Scripts/Main.js
+++ Scripts/Main.js
@@ -1,4 +1,30 @@
 document.querySelectorAll("a").forEach(link =>
 {
-    link.href = link.href.includes("wikipedia.org/") ? (link.href = link.href.includes("useskin=vector") ? link.href : link.href + (link.href.includes("?") ? "&" : "?") + 'useskin=vector') : link.href;
+    if (link.href.includes("wikipedia.org/"))
+    {
+        if (link.href.includes("useskin=vector"))
+        {
+            link.href = link.href;
+        }
+        else
+        {
+            if (link.href.includes("?"))
+            {
+                link.href = link.href + "&" + 'useskin=vector';
+            }
+            else if (link.href.includes("#"))
+            {
+                let splitURL = link.href.split('#', 2);
+                link.href = splitURL[0] + '?useskin=vector' + splitURL[1];
+            }
+            else
+            {
+                link.href = link.href + "?" + 'useskin=vector';
+            }
+        }
+    }
+    else
+    {
+        link.href = link.href;
+    }
 });
