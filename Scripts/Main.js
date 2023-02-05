522f0b4d4459ae74dd8ec5e602f6d5ba5ee5a1ed
nourHalhoul <nourhalhoul@gmail.com>
2023-02-05 08:46:46 -0500

    Implement redirect injection

--- Scripts/Main.js
+++ Scripts/Main.js
@@ -1,10 +1,17 @@
-// Check if the useskin parameter is set to vector
-var url = new URL(location.href);
-var useskin = url.searchParams.get("useskin");
-
-if (useskin !== "vector")
+chrome.webNavigation.onBeforeNavigate.addListener(function (details)
 {
-    // Apply the 2010 vector skin
-    url.searchParams.set("useskin", "vector");
-    location.href = url;
-}
+    // Check if the URL is a Wikipedia page
+    if (details.url.match(/^https:\/\/.*\.wikipedia.org\//))
+    {
+        // Check if the useskin parameter is set to vector
+        var url = new URL(details.url);
+        var useskin = url.searchParams.get("useskin");
+
+        if (useskin !== "vector")
+        {
+            // Apply the 2010 vector skin
+            url.searchParams.set("useskin", "vector");
+            chrome.tabs.update(details.tabId, { url: url.href });
+        }
+    }
+});
