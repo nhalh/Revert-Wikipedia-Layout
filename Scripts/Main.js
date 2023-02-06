3759289eb130cae6d0b29bb92160901afec8e461
nourHalhoul <nourhalhoul@gmail.com>
2023-02-06 17:44:39 -0500

    Rolling back version

--- Scripts/Main.js
+++ Scripts/Main.js
@@ -1,17 +1,10 @@
-chrome.webNavigation.onBeforeNavigate.addListener(function (details)
-{
-    // Check if the URL is a Wikipedia page
-    if (details.url.match(/^https:\/\/.*\.wikipedia.org\//))
-    {
-        // Check if the useskin parameter is set to vector
-        var url = new URL(details.url);
-        var useskin = url.searchParams.get("useskin");
+// Check if the useskin parameter is set to vector
+var url = new URL(location.href);
+var useskin = url.searchParams.get("useskin");
 
-        if (useskin !== "vector")
-        {
-            // Apply the 2010 vector skin
-            url.searchParams.set("useskin", "vector");
-            chrome.tabs.update(details.tabId, { url: url.href });
-        }
-    }
-});
+if (useskin !== "vector")
+{
+    // Apply the 2010 vector skin
+    url.searchParams.set("useskin", "vector");
+    location.href = url;
+}
